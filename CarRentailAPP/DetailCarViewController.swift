974b11a5c78e293002b57a0ff43e38cf0e181ef5
Alisa Ts <freiheiit92@mail.ru>
2021-11-24 20:28:23 +0300

    done VC's

---
+++ CarRentailAPP/DetailCarViewController.swift
@@ -0,0 +1,38 @@
+//
+//  DetailCarViewController.swift
+//  CarRentailAPP
+//
+//  Created by Alisa Ts on 24.11.2021.
+//
+
+import UIKit
+
+class DetailCarViewController: UIViewController {
+
+    @IBOutlet var imageCar: UIImageView!
+    @IBOutlet var carDescriptionLabel: UILabel!
+    
+    
+    var car: Car!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imageCar.image = UIImage(named: car.image)
+        carDescriptionLabel.text =
+        """
+            \(car.carNames) \n
+            Данный автомобиль имеет \(car.driveUnits) привод.
+            \(car.horsesPower) лошадиных сил.
+            Двигатель \(car.engine) л.
+            Коробка передач \(car.transmission) \n
+            Цена: \(car.price) в сутки
+        """
+    }
+    
+    // MARK: - Navigation
+//
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        guard let reserveVC = segue.destination as? ReserveViewController else { return }
+//        reserveVC.car = car
+//    }
+}
---
+++ CarRentailAPP/WelcomeViewController.swift
@@ -0,0 +1,27 @@
+//
+//  WelcomeViewController.swift
+//  CarRentailAPP
+//
+//  Created by Alisa Ts on 24.11.2021.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    @IBOutlet var headerImage: UIImageView!
+    @IBOutlet var textLabel: UILabel!
+    
+    private var carsList = Car.getCarList()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        headerImage.image = UIImage(named: "BMW X1")
+        textLabel.text =
+        """
+            Позвольте найти для вас идеальный автомобиль.\n
+            Подберем на любой вкус и кошелек, продолжительность проката от 1 часа до нескольких месяцев!\n
+            Бронируйте прямо в приложении
+        """
+    }
+}
